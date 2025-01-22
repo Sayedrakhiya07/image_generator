1afc32aa70c4d907d00f982bc88c601124d4b708
Sayedrakhiya07 <Sayedrakhiya07@gmail.com>
2025-01-22 20:05:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ImageGeneratorComponent } from "./image-generator/image-generator.component";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageGeneratorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/image-generator/image-generator.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-image-generator',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './image-generator.component.html',
+  styleUrl: './image-generator.component.scss',
+})
+export class ImageGeneratorComponent {
+
+  prompt: string = '';
+  generatedImageUrl: string | null = null;
+
+
+  generateImage() {
+    if (!this.prompt.trim()) {
+      alert('Please enter a prompt!');
+      return;
+    }
+
+    const payload = { prompt: this.prompt };
+    fetch('http://localhost:5000/generate-image', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to generate image');
+      }
+      return response.json();
+    }).then(data => {
+      this.generatedImageUrl = "D:\\Subhendu-Roy\\image_generation\\image_generation\\" + data.filename;
+    }).catch(error => {
+      console.error(error);
+      alert('Failed to generate image');
+    });
+
+  }
+}
